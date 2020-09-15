827503c94afb21482e255723c5c6dfae674c677c
grantfreyBU <54907819+grantfreyBU@users.noreply.github.com>
2020-09-15 11:22:17 -0500

    Ship Stats WIP update

--- SpaceUber/Assets/Scripts/ShipStats.cs
+++ SpaceUber/Assets/Scripts/ShipStats.cs
@@ -7,10 +7,13 @@
 
 using System;
 using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
 
 //[CreateAssetMenu(fileName = "new Ship Stats", menuName = "Ship Stats")]
-public class ShipStats : ScriptableObject
+public class ShipStats : MonoBehaviour
 {
+    [Header("Starting Ship Stats")]
     [SerializeField ,Tooltip("Starting amount of credits")]
     private int startingCredits;
     [SerializeField, Tooltip("Starting amount of energy")]
@@ -25,6 +28,17 @@ public class ShipStats : ScriptableObject
     private int startingFood;
     [SerializeField, Tooltip("Starting amount of ship health")]
     private int startingShipHealth;
+
+    [Header("Ship Stats UI")]
+    public TMP_Text creditsText;
+    public TMP_Text energyText;
+    public TMP_Text securityText;
+    public TMP_Text shipWeaponsText;
+    public TMP_Text crewText;
+    public TMP_Text foodText;
+    public TMP_Text shipHealthText;
+
+    List<GameObject> rooms;
 
     int credits { get; set; }
     int energyMax;
@@ -51,10 +65,18 @@ public class ShipStats : ScriptableObject
         foodPerTick = 0;
         shipHealthMax = startingShipHealth;
         shipHealthCurrent = startingShipHealth;
+
+        UpdateShipStatsUI();
     }
 
     public void UpdateShipStatsUI()
     {
-
+        creditsText.text = "Credits: " + credits.ToString();
+        energyText.text = "Energy: " + energyRemaining.ToString() + "/" + energyMax.ToString();
+        securityText.text = "Security: " + security.ToString();
+        shipWeaponsText.text = "Ship Weapons: " + shipWeapons.ToString();
+        crewText.text = "Crew: " + crewRemaining.ToString() + "/" + crewMax.ToString();
+        foodText.text = "Food: " + food.ToString() + " + " + foodPerTick.ToString();
+        shipHealthText.text = "Hull Durability: " + shipHealthCurrent.ToString() + "/" + shipHealthMax.ToString();
     }
 }
