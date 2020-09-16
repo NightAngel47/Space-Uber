ff2abcdd57a7e998b71ba79dc72fb439186bcfb0
grantfreyBU <54907819+grantfreyBU@users.noreply.github.com>
2020-09-16 15:57:58 -0500

    Added room stats and resouce scripts

---
+++ SpaceUber/Assets/Scripts/Resource.cs
@@ -0,0 +1,31 @@
+/*
+ * Resouce.cs
+ * Author(s): Grant Frey []
+ * Created on: 9/16/2020 (en-US)
+ * Description: 
+ */
+
+using NaughtyAttributes;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    [Dropdown("resourceTypes")]
+    public string resourceType;
+
+    private List<string> resourceTypes { get { return new List<string>() { "", "Credits", "Energy", "Security",
+        "Ship Weapons", "Crew", "Food", "Food Per Tick", "Hull Durability" }; } }
+
+    public int amount;
+
+    void Start()
+    {
+        GetComponent<RoomStats>().AddToResourceList(this);
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- SpaceUber/Assets/Scripts/ShipStats.cs
+++ SpaceUber/Assets/Scripts/ShipStats.cs
@@ -9,48 +9,65 @@ using System;
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using NaughtyAttributes;
 
 //[CreateAssetMenu(fileName = "new Ship Stats", menuName = "Ship Stats")]
 public class ShipStats : MonoBehaviour
 {
-    [Header("Starting Ship Stats")]
-    [SerializeField ,Tooltip("Starting amount of credits")]
+    [SerializeField ,Tooltip("Starting amount of credits"), Foldout("Starting Ship Stats")]
     private int startingCredits;
-    [SerializeField, Tooltip("Starting amount of energy")]
+    [SerializeField, Tooltip("Starting amount of energy"), Foldout("Starting Ship Stats")]
     private int startingEnergy;
-    [SerializeField, Tooltip("Starting value of security")]
+    [SerializeField, Tooltip("Starting value of security"), Foldout("Starting Ship Stats")]
     private int startingSecurity;
-    [SerializeField, Tooltip("Starting value of ship weapons")]
+    [SerializeField, Tooltip("Starting value of ship weapons"), Foldout("Starting Ship Stats")]
     private int startingShipWeapons;
-    [SerializeField, Tooltip("Starting amount of crew")]
+    [SerializeField, Tooltip("Starting amount of crew"), Foldout("Starting Ship Stats")]
     private int startingCrew;
-    [SerializeField, Tooltip("Starting amount of food")]
+    [SerializeField, Tooltip("Starting amount of food"), Foldout("Starting Ship Stats")]
     private int startingFood;
-    [SerializeField, Tooltip("Starting amount of ship health")]
+    [SerializeField, Tooltip("Starting amount of ship health"), Foldout("Starting Ship Stats")]
     private int startingShipHealth;
 
-    [Header("Ship Stats UI")]
+    [Foldout("Ship Stats UI Objects")]
     public TMP_Text creditsText;
+    [Foldout("Ship Stats UI Objects")]
     public TMP_Text energyText;
+    [Foldout("Ship Stats UI Objects")]
     public TMP_Text securityText;
+    [Foldout("Ship Stats UI Objects")]
     public TMP_Text shipWeaponsText;
+    [Foldout("Ship Stats UI Objects")]
     public TMP_Text crewText;
+    [Foldout("Ship Stats UI Objects")]
     public TMP_Text foodText;
+    [Foldout("Ship Stats UI Objects")]
     public TMP_Text shipHealthText;
 
-    List<GameObject> rooms;
+    private List<RoomStats> rooms;
 
-    int credits;
-    int energyMax;
-    int energyRemaining;
-    int security;
-    int shipWeapons;
-    int crewMax;
-    int crewRemaining;
-    int food;
-    int foodPerTick;
-    int shipHealthMax;
-    int shipHealthCurrent;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int credits;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int energyMax;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int energyRemaining;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int security;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int shipWeapons;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int crewMax;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int crewRemaining;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int food;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int foodPerTick;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int shipHealthMax;
+    [OnValueChanged("UpdateShipStatsUI")]
+    private int shipHealthCurrent;
 
     void Start()
     {
@@ -72,11 +89,54 @@ public class ShipStats : MonoBehaviour
     public void UpdateShipStatsUI()
     {
         creditsText.text = "Credits: " + credits.ToString();
-        energyText.text = "Energy: " + energyRemaining.ToString() + "/" + energyMax.ToString();
+        energyText.text = "Energy: " + energyRemaining.ToString() + " / " + energyMax.ToString();
         securityText.text = "Security: " + security.ToString();
         shipWeaponsText.text = "Ship Weapons: " + shipWeapons.ToString();
-        crewText.text = "Crew: " + crewRemaining.ToString() + "/" + crewMax.ToString();
+        crewText.text = "Crew: " + crewRemaining.ToString() + " / " + crewMax.ToString();
         foodText.text = "Food: " + food.ToString() + " + " + foodPerTick.ToString();
-        shipHealthText.text = "Hull Durability: " + shipHealthCurrent.ToString() + "/" + shipHealthMax.ToString();
+        shipHealthText.text = "Hull Durability: " + shipHealthCurrent.ToString() + " / " + shipHealthMax.ToString();
+    }
+
+    public void UpdateCreditsAmount(int creditAmount)
+    {
+        credits += creditAmount;
+    }
+
+    public void UpdateEnergyAmount(int energyAmount)
+    {
+        energyMax += energyAmount;
+        energyRemaining += energyAmount;
+    }
+
+    public void UpdateSecurityAmount(int securityAmount)
+    {
+        security += securityAmount;
+    }
+
+    public void UpdateShipWeaponsAmount(int shipWeaponsAmount)
+    {
+        shipWeapons += shipWeaponsAmount;
+    }
+
+    public void UpdateCrewAmount(int crewAmount)
+    {
+        crewMax += crewAmount;
+        crewRemaining += crewAmount;
+    }
+
+    public void UpdateFoodAmount(int foodAmount)
+    {
+        food += foodAmount;
+    }
+
+    public void UpdateFoodPerTickAmount(int foodPerTickAmount)
+    {
+        foodPerTick += foodPerTickAmount;
+    }
+
+    public void UpdateHullDurabilityAmount(int hullDurabilityAmount)
+    {
+        shipHealthMax += hullDurabilityAmount;
+        shipHealthCurrent += hullDurabilityAmount;
     }
 }
