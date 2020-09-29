fb7d8834b58fd269a3d414a71d56d392c254b5ae
Lachlan <LachlanRobertDuncan@gmail.com>
2020-09-29 05:12:27 -0500

    additive scene loaded is mostly working
    There is still one bug where it doesn't move items to the base scene the first time if you choose to do that because the added scene isn't loaded yet

--- SpaceUber/Assets/Scripts/AdditiveSceneManager.cs
+++ SpaceUber/Assets/Scripts/AdditiveSceneManager.cs
@@ -10,24 +10,44 @@ public class AdditiveSceneManager : MonoBehaviour
     public bool moveStartingObjects;
     
     private int addedSceneIndex;
+    private int baseSceneIndex;
     private bool addedSceneLoaded;
     private GameObject[] addedObjects;
     
     // Start is called before the first frame update
     void Start()
     {
+        baseSceneIndex = this.gameObject.scene.buildIndex;
+        
+        for(int i = SceneManager.sceneCount-1; i >= 0; i--)
+        {
+            int buildIndex = SceneManager.GetSceneAt(i).buildIndex;
+            if(buildIndex != baseSceneIndex && (buildIndex != startingAddedScene || !addSceneOnStart))
+            {
+                SceneManager.UnloadSceneAsync(buildIndex);
+            }
+        }
+        
+        addedSceneLoaded = false;
+        addedObjects = null;
+        
         if(addSceneOnStart)
         {
             LoadScene(startingAddedScene, moveStartingObjects);
         }
-        else
-        {
-            addedSceneLoaded = false;
-            addedObjects = null;
-        }
     }
     
-    public void LoadScene(int scene, bool moveObjects)
+    public void LoadSceneSeperate(int scene)
+    {
+        LoadScene(scene, false);
+    }
+    
+    public void LoadSceneMerged(int scene)
+    {
+        LoadScene(scene, true);
+    }
+    
+    private void LoadScene(int scene, bool moveObjects)
     {
         if(!SceneManager.GetSceneByBuildIndex(scene).isLoaded)
         {
@@ -38,13 +58,41 @@ public class AdditiveSceneManager : MonoBehaviour
             addedSceneLoaded = true;
             addedSceneIndex = scene;
         }
+        else if(scene != baseSceneIndex)
+        {
+            addedSceneIndex = scene;
+            addedSceneLoaded = true;
+        }
         
         if(moveObjects)
         {
-            addedObjects = SceneManager.GetSceneByBuildIndex(scene).GetRootGameObjects();
-            foreach(GameObject obj in addedObjects)
+            print(SceneManager.GetSceneByBuildIndex(scene).isLoaded);
+            GameObject[] newAddedObjects = SceneManager.GetSceneByBuildIndex(scene).GetRootGameObjects();
+            foreach(GameObject obj in newAddedObjects)
             {
-                SceneManager.MoveGameObjectToScene(obj, SceneManager.GetActiveScene());
+                SceneManager.MoveGameObjectToScene(obj, SceneManager.GetSceneByBuildIndex(baseSceneIndex));
+            }
+            
+            if(addedObjects != null)
+            {
+                GameObject[] oldAddedObjects = addedObjects;
+                int totalLength = newAddedObjects.Length + oldAddedObjects.Length;
+                addedObjects = new GameObject[totalLength];
+                for(int i = 0; i < addedObjects.Length; i++)
+                {
+                    if(i < oldAddedObjects.Length)
+                    {
+                        addedObjects[i] = oldAddedObjects[i];
+                    }
+                    else
+                    {
+                        addedObjects[i] = newAddedObjects[i-oldAddedObjects.Length];
+                    }
+                }
+            }
+            else
+            {
+                addedObjects = newAddedObjects;
             }
         }
     }
