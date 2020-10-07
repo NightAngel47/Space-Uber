e315efddcab0a35c08aec02d0f1ae532dfa8a3b5
sferstein <57074114+sferstein@users.noreply.github.com>
2020-10-07 18:09:02 -0500

    Updated ChoiceOutcomes

--- SpaceUber/Assets/Scripts/Event System/ChoiceOutcomes.cs
+++ SpaceUber/Assets/Scripts/Event System/ChoiceOutcomes.cs
@@ -2,7 +2,7 @@
  * ChoiceOutcomes.cs
  * Author(s): Sam Ferstein
  * Created on: 9/18/2020 (en-US)
- * Description: 
+ * Description:
  */
 
 using NaughtyAttributes;
@@ -16,6 +16,14 @@ public class ChoiceOutcomes : MonoBehaviour
     ShipStats shipStats;
     public List<ResourceType> resourcesChanged;
     public List<int> amountChanged;
+
+    public bool isRandomOutcome;
+    [ShowIf("isRandomOutcome")]
+    public List<float> probabilities;
+
+    private float outcomeChance;
+    private float choiceThreshold = 0f;
+
 
     public enum ResourceType
     {
@@ -42,46 +50,90 @@ public class ChoiceOutcomes : MonoBehaviour
     void Start()
     {
         shipStats = FindObjectOfType<ShipStats>();
+        outcomeChance = Random.Range(0f, 100f);
     }
 
     public void ChoiceChange()
     {
-        if(shipStats != null)
+        if (shipStats != null)
         {
-            for (int i = 0; i < resourcesChanged.Count; i++)
+            if (isRandomOutcome)
             {
-                switch (resourcesChanged[i])
+                for (int i = 0; i < probabilities.Count; i++)
                 {
-                    case ResourceType.Credits:
-                        shipStats.UpdateCreditsAmount(amountChanged[i]);
-                        break;
-                    case ResourceType.Energy:
-                        shipStats.UpdateEnergyAmount(amountChanged[i]);
-                        break;
-                    case ResourceType.Security:
-                        shipStats.UpdateSecurityAmount(amountChanged[i]);
-                        break;
-                    case ResourceType.ShipWeapons:
-                        shipStats.UpdateShipWeaponsAmount(amountChanged[i]);
-                        break;
-                    case ResourceType.Crew:
-                        shipStats.UpdateCrewAmount(amountChanged[i]);
-                        break;
-                    case ResourceType.Food:
-                        shipStats.UpdateFoodAmount(amountChanged[i]);
-                        break;
-                    case ResourceType.FoodPerTick:
-                        shipStats.UpdateFoodPerTickAmount(amountChanged[i]);
-                        break;
-                    case ResourceType.HullDurability:
-                        shipStats.UpdateHullDurabilityAmount(amountChanged[i]);
-                        break;
-                    default:
-                        break;
+                    choiceThreshold += probabilities[i];
+                    if (outcomeChance <= choiceThreshold)
+                    {
+                        switch (resourcesChanged[i])
+                        {
+                            case ResourceType.Credits:
+                                shipStats.UpdateCreditsAmount(amountChanged[i]);
+                                break;
+                            case ResourceType.Energy:
+                                shipStats.UpdateEnergyAmount(amountChanged[i]);
+                                break;
+                            case ResourceType.Security:
+                                shipStats.UpdateSecurityAmount(amountChanged[i]);
+                                break;
+                            case ResourceType.ShipWeapons:
+                                shipStats.UpdateShipWeaponsAmount(amountChanged[i]);
+                                break;
+                            case ResourceType.Crew:
+                                shipStats.UpdateCrewAmount(amountChanged[i]);
+                                break;
+                            case ResourceType.Food:
+                                shipStats.UpdateFoodAmount(amountChanged[i]);
+                                break;
+                            case ResourceType.FoodPerTick:
+                                shipStats.UpdateFoodPerTickAmount(amountChanged[i]);
+                                break;
+                            case ResourceType.HullDurability:
+                                shipStats.UpdateHullDurabilityAmount(amountChanged[i]);
+                                break;
+                            default:
+                                break;
+                        }
+                        return;
+                    }
                 }
+            }
+            else if (!isRandomOutcome)
+            {
+                for (int i = 0; i < resourcesChanged.Count; i++)
+                {
+                    switch (resourcesChanged[i])
+                    {
+                        case ResourceType.Credits:
+                            shipStats.UpdateCreditsAmount(amountChanged[i]);
+                            break;
+                        case ResourceType.Energy:
+                            shipStats.UpdateEnergyAmount(amountChanged[i]);
+                            break;
+                        case ResourceType.Security:
+                            shipStats.UpdateSecurityAmount(amountChanged[i]);
+                            break;
+                        case ResourceType.ShipWeapons:
+                            shipStats.UpdateShipWeaponsAmount(amountChanged[i]);
+                            break;
+                        case ResourceType.Crew:
+                            shipStats.UpdateCrewAmount(amountChanged[i]);
+                            break;
+                        case ResourceType.Food:
+                            shipStats.UpdateFoodAmount(amountChanged[i]);
+                            break;
+                        case ResourceType.FoodPerTick:
+                            shipStats.UpdateFoodPerTickAmount(amountChanged[i]);
+                            break;
+                        case ResourceType.HullDurability:
+                            shipStats.UpdateHullDurabilityAmount(amountChanged[i]);
+                            break;
+                        default:
+                            break;
+                    }
 
+                }
             }
         }
-        
+
     }
 }
