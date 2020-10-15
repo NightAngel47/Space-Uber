90361ed34f41cbde5ff19f00261175bde27a8a64
GBrandt <gbrandt@mail.bradley.edu>
2020-10-15 09:56:10 -0700

    Stabalize Energy Level mini game more polished and dynamic

--- SpaceUber/Assets/Scripts/MiniGame/EnergyLevelStabalizeMiniGame.cs
+++ SpaceUber/Assets/Scripts/MiniGame/EnergyLevelStabalizeMiniGame.cs
@@ -5,6 +5,7 @@
  * Description: 
  */
 
+using Boo.Lang;
 using System.Xml.Schema;
 using TMPro;
 using UnityEngine;
@@ -12,56 +13,44 @@ using UnityEngine.UI;
 
 public class EnergyLevelStabalizeMiniGame : MonoBehaviour
 {
-	[SerializeField] Slider slider1;
-	[SerializeField] Slider slider2;
-	[SerializeField] Slider slider3;
-	[SerializeField] MiniGameButton button1;
-	[SerializeField] MiniGameButton button2;
-	[SerializeField] MiniGameButton button3;
-	[SerializeField] MiniGameButton switch1;
-	[SerializeField] MiniGameButton switch2;
-	[SerializeField] MiniGameButton switch3;
+	[SerializeField] Slider[] sliders;
+	[SerializeField] MiniGameButton[] buttonSwitches;
 	[SerializeField] TMP_Text optimizationText;
-	float slider1Target;
-    float slider2Target;
-    float slider3Target;
-	int button1Target;
-	int button2Target;
-	int button3Target;
-	int switch1Target;
-	int switch2Target;
-	int switch3Target;
+	[SerializeField] GameObject[] powerBarIndicators;
+	[Tooltip("This is the percentage added to the total optimization")]
+	[SerializeField] uint difficultyAdjustment;
+	float[] sliderTargets;
+	int[] buttonSwitchTargets;
 
-	float optimization;
 
 	private void Start()
 	{
-		slider1Target = Random.Range(0f, 1f);
-		slider2Target = Random.Range(0f, 1f);
-		slider3Target = Random.Range(0f, 1f);
-		button1Target = Random.Range(0, 2);
-		button2Target = Random.Range(0, 2); 
-		button3Target = Random.Range(0, 2); 
-		switch1Target = Random.Range(0, 2); 
-		switch2Target = Random.Range(0, 2); 
-		switch3Target = Random.Range(0, 2); 
+		sliderTargets = new float[sliders.Length];
+		buttonSwitchTargets = new int[buttonSwitches.Length];
+		RandomizeTargets();
 	}
 
 	private void Update()
 	{
-		float sliderOptimizationLevel1 = 1- (Mathf.Abs(slider1Target - slider1.value));
-		float sliderOptimizationLevel2 = 1 - (Mathf.Abs(slider2Target - slider2.value));
-		float sliderOptimizationLevel3 = 1 - (Mathf.Abs(slider3Target - slider3.value));
-		float buttonOptimizationLevel1 = 1 - (Mathf.Abs(button1Target - button1.value));
-		float buttonOptimizationLevel2 = 1 - (Mathf.Abs(button2Target - button2.value));
-		float buttonOptimizationLevel3 = 1 - (Mathf.Abs(button3Target - button3.value));
-		float switchOptimizationLevel1 = 1 - (Mathf.Abs(switch1Target - switch1.value));
-		float switchOptimizationLevel2 = 1 - (Mathf.Abs(switch2Target - switch2.value));
-		float switchOptimizationLevel3 = 1 - (Mathf.Abs(switch3Target - switch3.value));
-		float total = sliderOptimizationLevel1 + sliderOptimizationLevel2 + sliderOptimizationLevel3 +
-			buttonOptimizationLevel1 + buttonOptimizationLevel2 + buttonOptimizationLevel3 + 
-			switchOptimizationLevel1 + switchOptimizationLevel2 + switchOptimizationLevel3;
-		optimization = Mathf.RoundToInt(100f * (total / 9f));
-		optimizationText.text = (optimization + "%");
+		List<float> optimizationLevels = new List<float>();
+		for (int i = 0; i < sliders.Length; i++) { optimizationLevels.Add(1.2f - (Mathf.Abs(sliderTargets[i] - sliders[i].value))); }
+		for (int i = 0; i < buttonSwitches.Length; i++) { optimizationLevels.Add(1 - (Mathf.Abs(buttonSwitchTargets[i] - buttonSwitches[i].value))); }
+		int total = 0;
+		//add the rounded 
+		foreach(float optimizationLevel in optimizationLevels) { total += Mathf.RoundToInt(100f * (optimizationLevel / (buttonSwitches.Length + sliders.Length))); }
+
+		//Make indicator number the percent of indicators active based on total
+		int indicatorNumber = (total / (100/powerBarIndicators.Length));
+
+		for(int i = 0; i < powerBarIndicators.Length; i++) { powerBarIndicators[i].SetActive(i < indicatorNumber); }
+
+		if(total > 100) { total = 100; }
+		optimizationText.text = (total + "%");
+	}
+
+	void RandomizeTargets()
+	{
+		for (int i = 0; i < sliders.Length; i++) { sliderTargets[i] = Random.Range(0f, 1f); }
+		for (int i = 0; i < buttonSwitches.Length; i++) { buttonSwitchTargets[i] = Random.Range(0, 2); }
 	}
 }
