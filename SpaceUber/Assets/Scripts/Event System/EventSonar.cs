162197f8039a3e16bdd29eef7fff8508eeed297a
Scott A <squinton99@gmail.com>
2020-10-29 14:57:29 -0500

    Event Timer nearly Set up, needs implementation with scene and event system

--- SpaceUber/Assets/Scripts/Event System/EventSonar.cs
+++ SpaceUber/Assets/Scripts/Event System/EventSonar.cs
@@ -8,21 +8,62 @@
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EventSonar : MonoBehaviour
 {
     public float spinRate = 5f;
-    public GameObject dot;
+
+    public List<GameObject> dots;
+    private List<Vector3> dotPositions;
+    [HideInInspector] public int dotsShown = 0;
+
     public GameObject bar;
+    public Transform anchor;
 
-    void ShowDot()
+    private void Start()
     {
-
+        
+        bar.transform.localPosition = Vector3.zero;
+        
+        for(int i = 0; i < dots.Count; i++)
+        {
+            dotPositions[i] = dots[i].transform.position;
+            HideDot(dots[i]);
+        }
     }
 
-    void HideDot()
+    private void FixedUpdate()
     {
 
+        //bar.transform.RotateAround(anchor.localPosition, Vector3.back, Time.deltaTime * spinRate);
+        bar.transform.Rotate(Vector3.back * Time.deltaTime * (360/spinRate));
+    }
+
+    public void ShowDot(GameObject dot)
+    {
+        dot.SetActive(true);
+        dot.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+        DotPulse(dot);
+    }
+
+    public void HideAllDots()
+    {
+        for (int i = 0; i < dots.Count; i++)
+        {
+            HideDot(dots[i]);
+        }
+    }
+    public void HideDot(GameObject dot)
+    {
+        dot.SetActive(false);
+    }
+
+    private void DotPulse(GameObject dot)
+    {
+        //decrease opacity
+        dot.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Mathf.Lerp(1, 0, spinRate));
+        ShowDot(dot);
     }
 
     public IEnumerator Spin()
