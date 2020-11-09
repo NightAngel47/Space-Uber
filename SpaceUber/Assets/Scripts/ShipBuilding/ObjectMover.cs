977a9333c49f3fa5ec4159aa866143719e4594bf
sidfoe <sidfoe2125@gmail.com>
2020-11-09 10:14:18 -0600

    Bug Fix: Soft lock when cannot afford rooms

--- SpaceUber/Assets/Scripts/ShipBuilding/ObjectMover.cs
+++ SpaceUber/Assets/Scripts/ShipBuilding/ObjectMover.cs
@@ -213,7 +213,7 @@ public class ObjectMover : MonoBehaviour
         else
         {
             Debug.Log("Cannot Afford");
-            //Destroy(gameObject);
+            Destroy(gameObject);
         }
     }
 
