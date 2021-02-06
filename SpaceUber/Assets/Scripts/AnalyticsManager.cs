0f11a2249c8b1c803107dae4a41860949c07bdbc
Steven <NightAngel47@users.noreply.github.com>
2021-02-06 15:16:15 -0600

    Updating Analytics Test for better data
    Matching using event name as key and choice name as value to match them in unity dashboard.

--- SpaceUber/Assets/Scripts/AnalyticsManager.cs
+++ SpaceUber/Assets/Scripts/AnalyticsManager.cs
@@ -2,7 +2,7 @@
  * AnalyticsManager.cs
  * Author(s): Steven Drovie []
  * Created on: 2/5/2021 (en-US)
- * Description: 
+ * Description: Handles collecting & sending analytics data using Unity Analytics.
  */
 
 using System.Collections.Generic;
@@ -11,13 +11,19 @@ using UnityEngine.Analytics;
 
 public static class AnalyticsManager
 {
+    /// <summary>
+    /// Collects and Sends data on the completed story and random event.
+    /// </summary>
+    /// <param name="gameEvent">The event that the player just completed.</param>
+    /// <param name="choice">The choice that the player made during the event.</param>
     public static void OnEventComplete(InkDriverBase gameEvent, EventChoice choice)
     {
         var eventType = gameEvent.isStoryEvent ? "storyEvent" : "randomEvent";
         Dictionary<string, object> data = new Dictionary<string, object>
         {
-            {"eventName", gameEvent.EventName}, 
-            {"choiceName", choice.ChoiceName}
+            {"eventName", gameEvent.EventName},
+            {"choiceName", choice.ChoiceName},
+            {gameEvent.EventName, choice.ChoiceName}
         };
 
         Analytics.CustomEvent(eventType, data);
