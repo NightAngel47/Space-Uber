4669eec4f5e6bd54ba199d269a9bd5c69be74f94
Frank Calabrese <57340460+FrankCalabrese@users.noreply.github.com>
2021-02-25 11:21:58 -0600

    Room buying roughly implemented

--- SpaceUber/Assets/Scripts/ShipBuilding/UI/ShipBuildingUIController.cs
+++ SpaceUber/Assets/Scripts/ShipBuilding/UI/ShipBuildingUIController.cs
@@ -1,3 +1,10 @@
+/* Frank Calabrese  
+ * ShipBuildingUIController.cs
+ * 2/25/21
+ * Sets rooms to be displayed on buy screen depending on tabs
+ * and spawns rooms
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,47 +13,136 @@ using UnityEngine.UI;
 
 public class ShipBuildingUIController : MonoBehaviour
 {
-    [SerializeField] GameObject roomPrefab;
+    public enum shipBuildingTab { Credits, Crew, Food, HullDurability, Power, ShipWeapons, Security }
+    shipBuildingTab tab;
 
-    [SerializeField] Sprite roomImage;
+    private GameObject roomPrefab;
+    private GameObject objectsToSpawn;
+
+    [SerializeField] Image roomImage;
     [SerializeField] TextMeshProUGUI rname;
     [SerializeField] TextMeshProUGUI needsCredits;
     [SerializeField] TextMeshProUGUI needsPower;
     [SerializeField] TextMeshProUGUI needsCrew;
-
     [SerializeField] TextMeshProUGUI producesResource;
     [SerializeField] TextMeshProUGUI producesAmount;
 
-    private Image roomImageTemp;
-    private string nameTemp;
-    private int needsCreditsTemp;
-    private int needsPowerTemp;
-    private int needsCrewTemp;
-
-    private string producesResourceTemp;
-    private int producesAmountTemp;
+    [SerializeField] GameObject roomArmory;
+    [SerializeField] GameObject roomArmorPlating;
+    [SerializeField] GameObject roomBrig;
+    [SerializeField] GameObject roomBunks;
+    [SerializeField] GameObject roomCoreChargingTerminal;
+    [SerializeField] GameObject roomEnergyCannon;
+    [SerializeField] GameObject roomHydroponics;
+    [SerializeField] GameObject roomMedbay;
+    [SerializeField] GameObject roomPantry;
+    [SerializeField] GameObject roomPhotonTorpedos;
+    [SerializeField] GameObject roomPowerCore;
+    [SerializeField] GameObject roomShieldGenerator;
+    [SerializeField] GameObject roomStorage;
+    [SerializeField] GameObject roomTeleportationStation;
+    [SerializeField] GameObject roomVIPLounge;
+    [SerializeField] GameObject roomWarpDrive;
 
     private void Start()
     {
-        nameTemp = roomPrefab.GetComponent<RoomStats>().roomName;
-        needsCreditsTemp = roomPrefab.GetComponent<RoomStats>().price;
-        needsPowerTemp = roomPrefab.GetComponent<RoomStats>().minPower;
-        needsCrewTemp = roomPrefab.GetComponent<RoomStats>().minCrew;
-        producesResourceTemp = roomPrefab.GetComponent<Resource>().resourceType.name;
-        producesAmountTemp = roomPrefab.GetComponent<Resource>().amount;
+        objectsToSpawn = GameObject.FindGameObjectWithTag("ObjectsToSpawn");
+        tab = shipBuildingTab.Credits;
 
     }
 
     private void Update()
     {
-        rname.text = nameTemp;
-        needsCredits.text = "" + needsCreditsTemp;
-        needsPower.text = "" + needsPowerTemp;
-        needsCrew.text = "" + needsCrewTemp;
-        producesResource.text = producesResourceTemp;
-        producesAmount.text = "" + producesAmountTemp;
+        UpdateRoomInfo();
+
+        
 
     }
 
-    
+    public void SpawnSelectedPrefab()
+    {
+        objectsToSpawn.GetComponent<SpawnObject>().SpawnRoom(roomPrefab);
+    }
+
+    private void UpdateRoomInfo()
+    {
+        roomImage.sprite = roomPrefab.GetComponent<Sprite>();
+
+        rname.text = roomPrefab.GetComponent<RoomStats>().roomName;
+        needsCredits.text = "" + roomPrefab.GetComponent<RoomStats>().price;
+        needsPower.text = "" + roomPrefab.GetComponent<RoomStats>().minPower;
+        needsCrew.text = "" + roomPrefab.GetComponent<RoomStats>().minCrew;
+        producesResource.text = roomPrefab.GetComponent<Resource>().resourceType.name;
+        producesAmount.text = "" + roomPrefab.GetComponent<Resource>().amount;
+    }
+
+    private void GetRoomsToDisplay()
+    {
+        switch(tab)
+        {
+            case shipBuildingTab.Credits:
+                roomPrefab = roomBrig;
+                break;
+            case shipBuildingTab.Crew:
+                roomPrefab = roomBunks;
+                break;
+            case shipBuildingTab.Food:
+                roomPrefab = roomHydroponics;
+                break;
+            case shipBuildingTab.HullDurability:
+                roomPrefab = roomArmorPlating;
+                break;
+            case shipBuildingTab.Power:
+                roomPrefab = roomCoreChargingTerminal;
+                break;
+            case shipBuildingTab.ShipWeapons:
+                roomPrefab = roomPhotonTorpedos;
+                break;
+            case shipBuildingTab.Security:
+                roomPrefab = roomArmory;
+                break;
+            default:
+                Debug.LogError("ShipBuildingUIController state is unset");
+                break;
+        }
+            
+    }
+
+    public void ToCreditsTab()
+    {
+        tab = shipBuildingTab.Credits;
+        GetRoomsToDisplay();
+    }
+    public void ToCrewTab()
+    {
+        tab = shipBuildingTab.Crew;
+        GetRoomsToDisplay();
+    }
+    public void ToFoodTab()
+    {
+        tab = shipBuildingTab.Food;
+        GetRoomsToDisplay();
+    }
+    public void ToHullDurabilityTab()
+    {
+        tab = shipBuildingTab.HullDurability;
+        GetRoomsToDisplay();
+    }
+    public void ToPowerTab()
+    {
+        tab = shipBuildingTab.Power;
+        GetRoomsToDisplay();
+    }
+    public void ToShipWeaponsTab()
+    {
+        tab = shipBuildingTab.ShipWeapons;
+        GetRoomsToDisplay();
+    }
+    public void ToSecurityTab()
+    {
+        tab = shipBuildingTab.Security;
+        GetRoomsToDisplay();
+    }
+
+
 }
