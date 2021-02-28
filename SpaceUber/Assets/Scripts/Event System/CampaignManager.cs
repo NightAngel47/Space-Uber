314d4f1e624487771be1fe83f4e25b87f6d1347b
Scott <squinton99@gmail.com>
2021-02-27 21:09:19 -0600

    Fixed final plan variables

--- SpaceUber/Assets/Scripts/Event System/CampaignManager.cs
+++ SpaceUber/Assets/Scripts/Event System/CampaignManager.cs
@@ -230,7 +230,7 @@ public class CampaignManager : MonoBehaviour
             TruthTold,
             ScienceSavior,
             KellisLoyalty,
-            EndgamePlan
+            LexaPlan
         }
 
         public bool ft_lexaDoomed;
@@ -238,6 +238,10 @@ public class CampaignManager : MonoBehaviour
         public bool ft_truthTold;
         public bool ft_scienceSavior;
         public bool ft_kellisLoyalty;
-        public bool ft_endgamePlan;
+        public bool ft_lexaPlan;
+        public bool ft_mateoPlan;
+        public bool ft_lanriRipleyPlan;
+        public bool ft_kuonPlan;
+
     }
 }
