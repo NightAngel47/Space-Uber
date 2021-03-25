fec27969d47dc7974f4660c7ffb7a232ca7cc628
sferstein <57074114+sferstein@users.noreply.github.com>
2021-03-25 09:31:58 -0500

    Couple of UI fixes and minor tweaks

--- SpaceUber/Assets/Scripts/ButtonTwoBehaviour.cs
+++ SpaceUber/Assets/Scripts/ButtonTwoBehaviour.cs
@@ -42,4 +42,9 @@ public class ButtonTwoBehaviour : MonoBehaviour
             }
         }
     }
+
+    public void GoToEvent()
+    {
+        button.onClick.AddListener(EventSystem.instance.SkipToEvent);
+    }
 }
