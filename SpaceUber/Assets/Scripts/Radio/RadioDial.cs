3b362e48be22766cbd107bbc9b361e393f7c25f1
Frank Calabrese <57340460+FrankCalabrese@users.noreply.github.com>
2021-03-25 00:55:20 -0500

    Finished new radio UI

--- SpaceUber/Assets/Scripts/Radio/RadioDial.cs
+++ SpaceUber/Assets/Scripts/Radio/RadioDial.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
-public class RadioDial : MonoBehaviour
+public class RadioDial : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Image myImage;
     [SerializeField] RectTransform rotator;
     [SerializeField] Slider slider;
+    [SerializeField] bool stationDial;
+    private bool isMouseOverObject;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +23,29 @@ public class RadioDial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        myImage.fillAmount = slider.value;
-        rotator.rotation = Quaternion.Euler(0f, 0f, - slider.value * 360);
+        if (Input.GetMouseButton(0) && isMouseOverObject)
+        {
+            Vector3 difference = rotator.transform.InverseTransformPoint(Input.mousePosition);
+            var angle = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
+            rotator.transform.Rotate(0f, 0f, -angle);
+        }
+
+        if (stationDial) slider.value = (Mathf.Abs(rotator.rotation.z) / 360f) * 100f * 20f;
+        else slider.value = (Mathf.Abs(rotator.rotation.z)/360f) * 100f * 4f;
+
+        if(!stationDial)myImage.fillAmount = slider.value;
+        
     }
+
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isMouseOverObject = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isMouseOverObject = false;
+    }
+
 }
