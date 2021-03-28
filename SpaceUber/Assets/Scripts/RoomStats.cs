8e24cbcbca95e0ea5696491cca22d31d834c4923
sidfoe <sidfoe2125@gmail.com>
2021-03-28 16:17:11 -0500

    Bug Fix: Resource UI are all correct
    room details panel and top bar display correct resource stat when changed

--- SpaceUber/Assets/Scripts/RoomStats.cs
+++ SpaceUber/Assets/Scripts/RoomStats.cs
@@ -346,7 +346,8 @@ public class RoomStats : MonoBehaviour
                 Debug.LogError("Resource type: " + resource.resourceType.resourceName + " not setup in RoomStats");
                 break;
         }
-        
+
+        FindObjectOfType<CrewManagementRoomDetailsMenu>().UpdateCrewAssignment(currentCrew);
         //AddOneRoomStat(resourceData);
     }
 
