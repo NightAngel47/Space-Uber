5454b3f833327d51ba7517ebea9ecd20d93ed850
Frank Calabrese <57340460+FrankCalabrese@users.noreply.github.com>
2021-04-13 09:54:24 -0500

    Made main menu music cooperate with audiomanager

---
+++ SpaceUber/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,25 @@
+/* Frank Calabrese
+ * 4/13/21
+ * MainMenu.cs
+ * things in this script happen when the main menu is loaded
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitUntil(() => AudioManager.instance != null);
+        AudioManager.instance.PlayMusicWithTransition("Main Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
