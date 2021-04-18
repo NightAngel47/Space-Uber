cc591ae151c319dc09e4d08bc0f267202fffd7e6
Scott <squinton99@gmail.com>
2021-04-18 09:02:35 -0500

    Erased the randomization aspects of this script

--- SpaceUber/Assets/Scripts/MiniGame/HullRepair/HullRepairMiniGame.cs
+++ SpaceUber/Assets/Scripts/MiniGame/HullRepair/HullRepairMiniGame.cs
@@ -23,85 +23,11 @@ public class HullRepairMiniGame : MiniGame
 	protected override void Start()
     {
 		base.Start();
-        FillLists();
-        RandomizePositions();
+        
         Tutorial.Instance.SetCurrentTutorial(7, true);
 	}
 
-	private void FillLists()
-    {
-		smallPiecesList = new List<GameObject>();
-		largePiecesList = new List<GameObject>();
-		smallSlotsList = new List<HullPieceSlot>();
-		largeSlotsList = new List<HullPieceSlot>();
-
-		//generate array sizes
-		int smallPieceCount = smallPieces.transform.childCount;
-		int largePieceCount = largePieces.transform.childCount;
-		int smallSlotCount = smallSlots.transform.childCount;
-		int largeSlotCount = largeSlots.transform.childCount;
-
-		//fill arrays
-		for (int i = 0; i < smallPieceCount; i++)
-        {
-			smallPiecesList.Add(smallPieces.transform.GetChild(i).gameObject);
-        }
-		for (int i = 0; i < largePieceCount; i++)
-		{
-			largePiecesList.Add(largePieces.transform.GetChild(i).gameObject);
-		}
-
-		for (int i = 0; i < smallSlotCount; i++)
-		{
-			smallSlotsList.Add(smallSlots.transform.GetChild(i).gameObject.GetComponent<HullPieceSlot>());
-		}
-		for (int i = 0; i < largeSlotCount; i++)
-		{
-			largeSlotsList.Add(largeSlots.transform.GetChild(i).gameObject.GetComponent<HullPieceSlot>());
-		}
-	}
-	private void RandomizePositions()
-    {
-		//ensure they are "taken" at initialization
-		foreach(HullPieceSlot slot in smallSlotsList)
-		{
-			slot.taken = false;
-        }
-		foreach (HullPieceSlot slot in largeSlotsList)
-		{
-			slot.taken = false;
-		}
-
-		foreach(GameObject piece in smallPiecesList)
-        {
-			int randNum = Random.Range(0, smallSlotsList.Count);
-			
-			while(smallSlotsList[randNum].taken)
-            {
-				randNum = Random.Range(0, smallSlotsList.Count);
-			}
-
-			smallSlotsList[randNum].taken = true;
-			piece.transform.position = smallSlotsList[randNum].gameObject.transform.position;
-			piece.GetComponent<HullPiece>().mySlot = smallSlotsList[randNum];
-
-		}
-
-		foreach (GameObject piece in largePiecesList)
-		{
-			int randNum = Random.Range(0, largeSlotsList.Count);
-
-			while (largeSlotsList[randNum].taken)
-			{
-				randNum = Random.Range(0, largeSlotsList.Count);
-			}
-
-			largeSlotsList[randNum].taken = true;
-			piece.transform.position = largeSlotsList[randNum].gameObject.transform.position;
-			piece.GetComponent<HullPiece>().mySlot = largeSlotsList[randNum];
-		}
-	}
-
+    
 
 
     private void Update()
@@ -127,4 +53,80 @@ public class HullRepairMiniGame : MiniGame
 
 	public Color CoveredColor() { return coveredColor; }
 	public Color UncoveredColor() { return uncoveredColor; }
+
+	#region RandomizePositions
+	//private void FillLists()
+	//{
+	//	smallPiecesList = new List<GameObject>();
+	//	largePiecesList = new List<GameObject>();
+	//	smallSlotsList = new List<HullPieceSlot>();
+	//	largeSlotsList = new List<HullPieceSlot>();
+
+	//	//generate array sizes
+	//	int smallPieceCount = smallPieces.transform.childCount;
+	//	int largePieceCount = largePieces.transform.childCount;
+	//	int smallSlotCount = smallSlots.transform.childCount;
+	//	int largeSlotCount = largeSlots.transform.childCount;
+
+	//	//fill arrays
+	//	for (int i = 0; i < smallPieceCount; i++)
+	//	{
+	//		smallPiecesList.Add(smallPieces.transform.GetChild(i).gameObject);
+	//	}
+	//	for (int i = 0; i < largePieceCount; i++)
+	//	{
+	//		largePiecesList.Add(largePieces.transform.GetChild(i).gameObject);
+	//	}
+
+	//	for (int i = 0; i < smallSlotCount; i++)
+	//	{
+	//		smallSlotsList.Add(smallSlots.transform.GetChild(i).gameObject.GetComponent<HullPieceSlot>());
+	//	}
+	//	for (int i = 0; i < largeSlotCount; i++)
+	//	{
+	//		largeSlotsList.Add(largeSlots.transform.GetChild(i).gameObject.GetComponent<HullPieceSlot>());
+	//	}
+	//}
+	//private void RandomizePositions()
+	//{
+	//	//ensure they are "taken" at initialization
+	//	foreach (HullPieceSlot slot in smallSlotsList)
+	//	{
+	//		slot.taken = false;
+	//	}
+	//	foreach (HullPieceSlot slot in largeSlotsList)
+	//	{
+	//		slot.taken = false;
+	//	}
+
+	//	foreach (GameObject piece in smallPiecesList)
+	//	{
+	//		int randNum = Random.Range(0, smallSlotsList.Count);
+
+	//		while (smallSlotsList[randNum].taken)
+	//		{
+	//			randNum = Random.Range(0, smallSlotsList.Count);
+	//		}
+
+	//		smallSlotsList[randNum].taken = true;
+	//		piece.transform.position = smallSlotsList[randNum].gameObject.transform.position;
+	//		piece.GetComponent<HullPiece>().mySlot = smallSlotsList[randNum];
+
+	//	}
+
+	//	foreach (GameObject piece in largePiecesList)
+	//	{
+	//		int randNum = Random.Range(0, largeSlotsList.Count);
+
+	//		while (largeSlotsList[randNum].taken)
+	//		{
+	//			randNum = Random.Range(0, largeSlotsList.Count);
+	//		}
+
+	//		largeSlotsList[randNum].taken = true;
+	//		piece.transform.position = largeSlotsList[randNum].gameObject.transform.position;
+	//		piece.GetComponent<HullPiece>().mySlot = largeSlotsList[randNum];
+	//	}
+	//}
+	#endregion
 }
